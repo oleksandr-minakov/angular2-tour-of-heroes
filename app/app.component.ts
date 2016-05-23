978fcad290935be30ded6dd0af709ae83b546da8
Oleksandr Minakov <oleksandr.minakov@gmail.com>
2016-05-23 19:26:41 +0300

    Add dashboard
    Add dashboard and change styles.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component }       from '@angular/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
-import { HeroesComponent } from "./heroes.component";
-import { HeroService } from "./hero.service";
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component'
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
     selector: 'my-app',
     template: `
       <h1>{{title}}</h1>
-      <a [routerLink]="['Heroes']">Heroes</a>
+      <nav>
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+      </nav>
       <router-outlet></router-outlet>
     `,
+    styleUrls:  ['app/app.component.css'],
     directives: [ROUTER_DIRECTIVES],
     providers: [
         ROUTER_PROVIDERS,
@@ -22,7 +28,18 @@ import { HeroService } from "./hero.service";
         path: '/heroes',
         name: 'Heroes',
         component: HeroesComponent
-    }
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
+    },
 ])
 
 export class AppComponent {
