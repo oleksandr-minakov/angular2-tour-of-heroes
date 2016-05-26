5db0bdcfb70d52511def0077c91eebb3d25bcfd9
Oleksandr Minakov <oleksandr.minakov@gmail.com>
2016-05-26 12:55:15 +0300

    Add operations over hero to UI

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -4,16 +4,21 @@ import { Router } from '@angular/router-deprecated';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service'
+import { HeroDetailComponent } from './hero-detail.component';
 
 @Component({
     selector: 'my-heroes',
     templateUrl: 'app/heroes.component.html',
     styleUrls:  ['app/heroes.component.css'],
+    directives: [HeroDetailComponent],
 
 })
 export class HeroesComponent implements OnInit {
-    public heroes: Hero[];
+    heroes: Hero[];
     selectedHero: Hero;
+    addingHero = false;
+    error: any;
+
     constructor(
         private router: Router,
         private heroService: HeroService) {
@@ -21,6 +26,27 @@ export class HeroesComponent implements OnInit {
 
     getHeroes() {
         this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    addHero() {
+        this.addingHero = true;
+        this.selectedHero = null;
+    }
+
+    close(savedHero: Hero) {
+        this.addingHero = false;
+        if (savedHero) { this.getHeroes(); }
+    }
+
+    delete(hero: Hero, event: any) {
+        event.stopPropagation();
+        this.heroService
+            .delete(hero)
+            .then(() => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) { this.selectedHero = null; }
+            })
+            .catch(error => this.error = error); // TODO: Display error message
     }
 
     ngOnInit() {
